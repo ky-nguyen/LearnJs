41d663ac640ae342affa8742386130435be2140b
ky-nguyen <kynguyen00710011999@gmail.com>
2023-02-19 18:08:58 +0700

    first commit

---
+++ consoleLog.js
@@ -0,0 +1,19 @@
+let a = 10;
+// console.log(a);
+
+var cheese = 'yummy';
+// console.log(cheese);
+
+var myNum = 5;
+// console.log(myNum);
+
+var myOtherNumber = 10;
+var sum = myNum + myOtherNumber
+// console.log(sum);
+
+var notNum = '5';
+var newNum = 10;
+
+// console.log(notNum + newNum);
+// console.log(parseInt(notNum) + newNum);
+
---
+++ typeOf.js
@@ -0,0 +1,14 @@
+var myNum = 10;
+console.log(typeof myNum);
+
+var myNumAsString = '5';
+console.log(typeof myNumAsString);
+
+var unfinedvariable;
+console.log(typeof unfinedvariable);
+
+var nothingAtall = null;
+console.log(typeof nothingAtall);
+
+var emtyString = "";
+console.log(typeof emtyString);
